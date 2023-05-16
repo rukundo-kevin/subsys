e77db2830328d2c5886c741eb6434f9b2bb3e3c3
Rukundo Kevin <kevinrukundo1@gmail.com>
2023-05-16 06:00:18 +0200

    fix: linting in setup
    - Add husky, lint-staged, commitlint
    - configure pre-commit, pre-push, commit-msg hooks

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
-import express from "express";
-import helmet from "helmet";
-import compression from "compression";
-import cors from "cors";
-import httpStatus from "http-status";
+import express from 'express';
+import helmet from 'helmet';
+import compression from 'compression';
+import cors from 'cors';
+import httpStatus from 'http-status';
 
-import routes from "./routes/";
-import ApiError from "./utils/ApiError";
-
+import routes from './routes/';
+import ApiError from './utils/ApiError';
 const app = express();
 
 // set security HTTP headers
@@ -23,14 +22,14 @@ app.use(compression());
 
 // enable cors
 app.use(cors());
-app.options("*", cors());
+app.options('*', cors());
 
 //  api routes
-app.use("/", routes);
+app.use('/', routes);
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
-  next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 });
 
 export default app;
--- src/routes/docs.route.ts
+++ src/routes/docs.route.ts
@@ -1,21 +1,21 @@
-import express from "express";
-import swaggerJsdoc from "swagger-jsdoc";
-import swaggerUi from "swagger-ui-express";
-import swaggerDefinition from "../../docs/swaggerDef";
+import express from 'express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDefinition from '../../docs/swaggerDef';
 
 const router = express.Router();
 
 const specs = swaggerJsdoc({
   swaggerDefinition,
-  apis: ["docs/*.yml"],
+  apis: ['docs/*.yml']
 });
 
-router.use("/", swaggerUi.serve);
+router.use('/', swaggerUi.serve);
 
 router.get(
-  "/",
+  '/',
   swaggerUi.setup(specs, {
-    explorer: true,
+    explorer: true
   })
 );
 
