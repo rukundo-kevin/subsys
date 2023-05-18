b39e57413e82c88bb7e6e40d919bcc1be44b34dc
Rukundo Kevin <kevinrukundo1@gmail.com>
2023-05-18 14:33:08 +0200

    feat(create-student): add create student feature
    - add student route, controller, service, validation and documentation
    - add student model
    - Test student creation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,10 @@ import config from '../config/config';
 import docsRoute from './docs.route';
 import authRoute from './auth.route';
 import studentRoute from './student.route';
+<<<<<<< HEAD
 import lecturerRoute from './lecturer.route';
+=======
+>>>>>>> f082e34 (feat(create-student): add create student feature)
 
 const router = express.Router();
 
