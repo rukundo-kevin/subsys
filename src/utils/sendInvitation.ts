388ae4a22b24bc569c249a26d1517029a3c19a13
TuyizeeAnastase <42033331+TuyizeeAnastase@users.noreply.github.com>
2023-05-26 07:20:44 +0200

    create lecturer invitation

--- src/utils/sendInvitation.ts
+++ src/utils/sendInvitation.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 
 
-export const sendEmails=(emails:string[],studentIds:string[],password:string,activationToken:any)=>{
+export const sendEmails=(emails:string[],studentIds:string[],type:string,password:string,activationToken:any)=>{
 const token=activationToken.activate.token
 const transporter=nodemailer.createTransport(
     {
@@ -32,11 +32,11 @@ try {
                   <h2 style="color:#FFFFFF;">Assign IT</h2>
                   </div>
                   <h4 style="margin-bottom:8px;color:#000000;">Hi there!</h4>
-                  <p style="color:#000000;">We are excited to welcome you to our online platform! You have been added as a student.
-                  Below is your password and student id. Click on the claim button to get started.</p>
+                  <p style="color:#000000;">We are excited to welcome you to our online platform! You have been added as a ${type}.
+                  Below is your password and ${type} id. Click on the claim button to get started.</p>
                   <ul>
                   <li style="color:#363143;">Generated password: ${password} <a href='${process.env.BASE_URL}/api/auth/activate/:${token}' style="color:##5D34EC;text-decoration: none; font-size: 20px;">Login</a></li>
-                  <li style="color:#363143;">Student ID is ${studentId}.</li>
+                  <li style="color:#363143;"> ${type} ID is ${studentId}.</li>
                   </ul>
                   <h4>Best regards,</h4>
                   <div style="border-radius: 10px;width:200px;height:40px;background-color:#5D34EC;text-align: center;">
